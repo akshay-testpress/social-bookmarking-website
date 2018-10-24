0aa15f876e62b802399c16eefec8bc2980c1c7bb
akshay-testpress <akshaykumar@testpress.in>
2018-10-24 11:28:17 +0530

    CHANGED THE RELATION FROM CASCADE TO PROTECT
    As deleting any director, gener or studio we would have lost the movie.

--- movies/models.py
+++ movies/models.py
@@ -1,8 +1,6 @@
 from django.db import models
 # Create your models here.
 
-
-
 class MoviesGenres(models.Model):
     title = models.CharField(max_length=100,null=False,unique=True)
 
@@ -44,10 +42,6 @@ class MoviesStudios(models.Model):
     class Meta:
         verbose_name_plural = "MoviesStudios"
 
-    def get_fields(self):
-        return [(field.name, field.value_to_string(self)) for field in MoviesStudios._meta.fields]
-
-
     def __str__(self):
         return self.title
 
@@ -55,9 +49,9 @@ class MoviesMaster(models.Model):
     title = models.CharField(max_length=100)
     prefix = models.CharField(max_length=100,null=True,blank=True) 
     sub_title = models.CharField(max_length=100,blank=True)
-    genre = models.ForeignKey(MoviesGenres,on_delete=models.CASCADE)
-    directors = models.ForeignKey(MoviesDirectors,on_delete=models.CASCADE)
-    studio = models.ForeignKey(MoviesStudios,on_delete=models.CASCADE)
+    genre = models.ForeignKey(MoviesGenres,on_delete=models.PROTECT)
+    directors = models.ForeignKey(MoviesDirectors,on_delete=models.PROTECT)
+    studio = models.ForeignKey(MoviesStudios,on_delete=models.PROTECT)
     release_date = models.DateField()
     cover_image = models.CharField(max_length=100,blank=True)  #will keep address of the field
     review = models.CharField(max_length=100)
